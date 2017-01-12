2be9d26aaf587cd3132e9c036a31a3f2c957be09
Nils Lundquist <nils@bitovi.com>
2017-01-11 17:01:04 -0700

    Add jqWidgets grid example

--- src/components/jqwidgetsgrid-table/jqwidgetsgrid-table.js
+++ src/components/jqwidgetsgrid-table/jqwidgetsgrid-table.js
@@ -3,8 +3,45 @@ import './jqwidgetsgrid-table.less!';
 import template from './jqwidgetsgrid-table.stache!';
 import ViewModel from './jqwidgetsgrid-table.viewmodel';
 
+import 'jqwidgets-framework/jqwidgets/styles/jqx.base.css!';
+import 'jquery';
+import 'jqwidgets-framework/jqwidgets/jqxcore';
+import 'jqwidgets-framework/jqwidgets/jqxdata';
+import 'jqwidgets-framework/jqwidgets/jqxbuttons';
+import 'jqwidgets-framework/jqwidgets/jqxscrollbar';
+import 'jqwidgets-framework/jqwidgets/jqxmenu';
+import 'jqwidgets-framework/jqwidgets/jqxlistbox';
+import 'jqwidgets-framework/jqwidgets/jqxdropdownlist';
+import 'jqwidgets-framework/jqwidgets/jqxgrid';
+import 'jqwidgets-framework/jqwidgets/jqxgrid.selection';
+import 'jqwidgets-framework/jqwidgets/jqxgrid.columnsresize';
+import 'jqwidgets-framework/jqwidgets/jqxgrid.filter';
+import 'jqwidgets-framework/jqwidgets/jqxgrid.sort';
+import 'jqwidgets-framework/jqwidgets/jqxgrid.pager';
+import 'jqwidgets-framework/jqwidgets/jqxgrid.grouping';
+
 export default Component.extend({
   tag: 'jqwidgetsgrid-table',
   viewModel: ViewModel,
-  template
+  template,
+  events: {
+    "inserted": function(){
+      this.$el = $(this.element);
+      this.initJQWidgetsTable();
+    },
+
+    initJQWidgetsTable(){
+      let derivedColumns = this.viewModel.attr("tableColumns"),
+          derivedAdapter = this.viewModel.attr("dataAdapter");
+
+      this.$el.jqxGrid({
+        source: derivedAdapter,
+        columns: derivedColumns,
+        sortable: true,
+        filterable: true,
+        pageable: true,
+        autoheight: true
+      });
+    }
+  }
 });
--- src/components/jqwidgetsgrid-table/jqwidgetsgrid-table.viewmodel.js
+++ src/components/jqwidgetsgrid-table/jqwidgetsgrid-table.viewmodel.js
@@ -3,8 +3,33 @@ import 'can/map/define/';
 
 export default Map.extend({
   define: {
-    message: {
-      value: 'This is the jqwidgetsgrid-table component'
+    tableData: {
+      value: {}
+    },
+    tableColumns: {
+      get(){
+        let tableData = this.attr("tableData");
+        return tableData.attr("headers").map((h, i) => {
+          return {
+            datafield: i,
+            text: "header-" + i,
+            pinned: i < 3,
+            width: 100
+          }
+        });
+      }
+    },
+    dataAdapter: {
+      get(){
+        let tableData = this.attr("tableData"),
+            source = {localdata: tableData.attr('rows'), datatype: 'array'},
+            dataAdapter = new $.jqx.dataAdapter(source, {
+              loadComplete: function (data) {},
+              loadError: function (xhr, status, error) {}
+            });
+
+        return dataAdapter;
+      }
     }
   }
 });
